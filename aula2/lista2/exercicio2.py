ce4c877b7aaafcdd44a8effd53325a7eef59fc42
Isaque-SL <isaque.s@escolar.ifrn.edu.br>
2025-04-15 12:57:03 +0000

    exercicio 2 completo

---
+++ aula2/lista2/exercicio2.py
@@ -0,0 +1,3 @@
+nota1 = int(input("Digite a nota do primeiro bimestre da disciplina:\n"))
+nota2 = int(input("Digite a nota do segundo bimestre da disciplina:\n"))
+print(f"Média parcial = {((nota1 * 2) + (nota2 * 3)) // 5}")
