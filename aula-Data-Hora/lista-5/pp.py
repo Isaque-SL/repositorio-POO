c8f48697f381b1b071800744b01c5710161b72e5
Isaque-SL <isaque.s@escolar.ifrn.edu.br>
2025-07-10 14:48:14 +0000

    paciente.py iniciado

---
+++ aula-Data-Hora/lista-5/pp.py
@@ -0,0 +1,16 @@
+from datetime import datetime
+
+class Paciente:
+    def __init__(self, nome, cpf, tele, nasc):
+        self.__ = nome
+        self.__ = cpf
+        self.__ = tele
+        self.__ = nasc
+
+    def Idade(self):
+        
+
+
+
+        return datetime.fromisoformat(self.__nasc)
+  
